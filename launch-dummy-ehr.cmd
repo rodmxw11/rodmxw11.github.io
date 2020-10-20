97c2764db237cee590aa31dbee1415036173ccd6
rmaxwell@us.ibm.com <rmaxwell@us.ibm.com>
2020-10-20 14:06:53 -0400

    Fixup ehr launch script

--- launch-dummy-ehr.cmd
+++ launch-dummy-ehr.cmd
@@ -1,11 +1,15 @@
 @setlocal
 @echo off
 
+@rem -- See: https://en.wikipedia.org/wiki/Percent-encoding#Percent-encoding_reserved_characters
+set encode_question=%%3F
+set encode_pound=%%23
+
 set dummy_ehr_url=https://rodmxw11.github.io
 set pwa_url=https://mdx-pwa-dev-personal-3.cmdx-k8s-2nodes-1zone.us-south.containers.appdomain.cloud
 set /p jwt_token=<jwt.txt
 
-set ehr_launch_url=%dummy_ehr_url%?pwa_iframe_url=%pwa_url%/#/gateway?jws=%jwt_token%
+set ehr_launch_url=%dummy_ehr_url%?pwa_iframe_url=%pwa_url%/%encode_pound%/gateway%encode_question%jws=%jwt_token%
 
 echo %ehr_launch_url%
 
